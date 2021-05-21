1476b0aa6591aa497ed6eee92f241daa1d2aa747
Dulfite <61243073+Dulfite@users.noreply.github.com>
2021-05-21 08:33:00 -0500

    Finished 8 tests

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,16 +1,53 @@
 package test;
 
 import org.junit.Test;
+import main.BalancedBrackets;
 
 import static org.junit.Assert.*;
 
 public class BalancedBracketsTest {
 
     //TODO: add tests here
-    @Test
+
+
+//    Test 1
+@Test
     public void emptyTest() {
-        assertEquals(true, true);
-    }
-
-
+    assertEquals(true, true);
+}
+//    Test 2
+@Test
+    public void onlyBracketsReturnTrue(){
+    assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+}
+//    Test 3
+@Test
+    public void onlyLeftOneBracketsReturnsFalse(){
+    assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+}
+//    Test 4
+@Test
+    public void onlyRightOneBracketsReturnFalse(){
+    assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+}
+//    Test 5
+@Test
+    public void stringBeforeBracketsReturnsTrue(){
+    assertTrue(BalancedBrackets.hasBalancedBrackets("For the Emperor![]"));
+}
+//    Test 6
+@Test
+    public void stringBehindBracketsReturnsTrue(){
+    assertTrue(BalancedBrackets.hasBalancedBrackets("[]For the Emperor!"));
+}
+//    Test 7
+@Test
+    public void stringMixBracketsReturnsTrue(){
+    assertTrue(BalancedBrackets.hasBalancedBrackets("For the Emperor[Bobby]."));
+}
+//    Test 8
+@Test
+    public void stringMixTwoBracketsReturnsTrue(){
+    assertTrue(BalancedBrackets.hasBalancedBrackets("For [the] Emperor[Bobby]!"));
+}
 }
